3101166c999bf4a610c4ebfa584903a47e006cac
M <supsune2@gmail.com>
2022-02-03 13:05:48 +0000

    Update Library.py

--- Library.py
+++ Library.py
@@ -84,84 +84,91 @@ Motor1 = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
 Motor2 = DRV8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
 
 def pitch_mirror(c):
-global direction
-direction = ""
-Motor2.SetMicroStep('hardward','fullstep')
-if (c >= 0):
-direction = "forward"
-elif (c < 0):
-direction = "backward"
-c *= -1
+  global direction
+  direction = ""
+  Motor2.SetMicroStep('hardward','fullstep')
+  
+  if (c >= 0):
+    direction = "forward"
+  elif (c < 0):
+    direction = "backward"
+    c *= -1
 
-if (direction == "forward"):
-setText_norefresh("up: " + str(c) + " steps " + "/ " + str(round(((360/2048)*c),2)) + " deg")
-else:
-setText_norefresh("down: -" + str(c) + " steps " + "/ -" + str(round(((360/2048)*c),2)) + " deg")
+  if (direction == "forward"):
+    setText_norefresh("up: " + str(c) + " steps " + "/ " + str(round(((360/2048)*c),2)) + " deg")
+  else:
+    setText_norefresh("down: -" + str(c) + " steps " + "/ -" + str(round(((360/2048)*c),2)) + " deg")
 
-Motor2.TurnStep(Dir = direction, steps = c, stepdelay = 0.001)
-Motor2.Stop()
+  Motor2.TurnStep(Dir = direction, steps = c, stepdelay = 0.001)
+  Motor2.Stop()
+
 def turn_mirror(c):
-global direction
-direction = ""
-Motor1.SetMicroStep('hardward','fullstep')
-if (c >= 0):
-direction = "forward"
-elif (c < 0):
-direction = "backward"
-c *= -1
+  global direction
+  direction = ""
+  Motor1.SetMicroStep('hardward','fullstep')
+  
+  if (c >= 0):
+    direction = "forward"
+  elif (c < 0):
+    direction = "backward"
+    c *= -1
 
-if (direction == "forward"):
-setText_norefresh("l/r: " + str(c) + " steps " + "/ " + str(round(((360/2048)*c),2)) + " deg")
-else:
-setText_norefresh("l/r: -" + str(c) + " steps " + "/ -" + str(round(((360/2048)*c),2)) + " deg")
+  if (direction == "forward"):
+    setText_norefresh("l/r: " + str(c) + " steps " + "/ " + str(round(((360/2048)*c),2)) + " deg")
+  else:
+    setText_norefresh("l/r: -" + str(c) + " steps " + "/ -" + str(round(((360/2048)*c),2)) + " deg")
 
-Motor1.TurnStep(Dir = direction, steps = c, stepdelay = 0.001)
-Motor1.Stop()
+  Motor1.TurnStep(Dir = direction, steps = c, stepdelay = 0.001)
+  Motor1.Stop()
 
-def stop_motors():
-Motor1.Stop()
-Motor2.Stop()
+ def stop_motors():
+  Motor1.Stop()
+  Motor2.Stop()
 
-"""pitch_mirror(-512)
+"""
+pitch_mirror(-512)
 turn_mirror(512)
 pitch_mirror(512)
-turn_mirror(-512)"""
+turn_mirror(-512)
+"""
 
 
 # laser functionailty
 GPIO.setup(26, GPIO.OUT)
 
 def laser_on():
-GPIO.output(26, GPIO.HIGH)
+  GPIO.output(26, GPIO.HIGH)
 
 def laser_off():
-GPIO.output(26, GPIO.LOW)
+  GPIO.output(26, GPIO.LOW)
 
 def laser(t):
-GPIO.output(26, GPIO.HIGH)
-time.sleep(t)
-GPIO.output(26, GPIO.LOW)
+  GPIO.output(26, GPIO.HIGH)
+  time.sleep(t)
+  GPIO.output(26, GPIO.LOW)
 
 
-"""ct = time.time()
+"""
+ct = time.time()
 while (time.time() <= (ct+5)):
-laser(0.05)
-time.sleep(0.05)"""
+  laser(0.05)
+  time.sleep(0.05)
+"""
 
 
 
 
 # lcd functionality
 def write_lcd(text):
-setText_norefresh(text)
+  setText_norefresh(text)
 
 # old
 # def lcd_colour(rgb):
-# rgb_list = rgb.split(",")
-# setRGB(int(rgb_list[0]), int(rgb_list[1]), int(rgb_list[2]))
+#   rgb_list = rgb.split(",")
+#   setRGB(int(rgb_list[0]), int(rgb_list[1]), int(rgb_list[2]))
 
 def lcd_colour(r, g, b):
-setRGB(r, g, b)
+  setRGB(r, g, b)
 
 """
 write_lcd("Procedures end")
